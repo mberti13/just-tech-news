762dba1ba3c888a7dde16da424b4301569e13b32
MJB <matt.berti13@gmail.com>
2022-04-26 14:58:00 -0500

    added verification of hashed password at login

--- models/User.js
+++ models/User.js
@@ -8,7 +8,13 @@ const sequelize = require('../config/connection');
 
 
 // * create User Model
-class User extends Model{}
+class User extends Model{
+
+    // * set up method to run on instance data (per user) to check password
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw, this.password);
+    } 
+}
 
 // * define table columns and config
 User.init(
@@ -60,7 +66,7 @@ User.init(
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
               },
-              
+
             // * set up beforeUpdate lifecycle "hook" functionality
             async beforeUpdate(updatedUserData){
                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
