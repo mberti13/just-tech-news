f08494b1e23f42ea7dc4d1716a0876c8b6b1bd7c
MJB <matt.berti13@gmail.com>
2022-04-29 15:33:45 -0500

    created vote model and associations

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
-// * create associations between User and Post
+// ! create associations between User and Post
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -10,7 +11,40 @@ Post.belongsTo(User,{
     foreignKey: 'user_id',
 });
 
+// ! Create associations between user and post in context with the voting system
+User.belongsToMany(Post,{
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User,{
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+// ! Individual Associations
+Vote.belongsTo(User,{
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post,{
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote,{
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote,{
+    foreignKey: 'post_id'
+});
+
+
+
 module.exports = {
     User,
-    Post
+    Post,
+    Vote
 };
