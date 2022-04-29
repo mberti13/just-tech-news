b234c730d05cd05d57035d75504a3887728b1418
MJB <matt.berti13@gmail.com>
2022-04-29 07:34:20 -0500

    created get routes for post model

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User,{
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
 });
 
 module.exports = {
---
+++ routes/api/post-routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+
+const {Post, User} = require('../../models');
+
+//get all Posts
+router.get('/', (req, res) =>{
+    console.log('=============');
+    Post.findAll({
+        // * Query Configuration
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => res.json(dbPostData))
+    .catch(err =>{
+        res.status(500).json(err);
+    });
+});
+
+// GET a single post
+router.get('/:id', (req, res) =>{
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        if(!dbPostData){
+            res.status(400).json({ message: 'No post found with this id. '});
+            return;
+        }
+        res.json(dbPostData);
+    }).catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+module.exports = router;
