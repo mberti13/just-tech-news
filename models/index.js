6ac2f5ee5b208347c8d3ff45be5249b2fd15eb27
MJB <matt.berti13@gmail.com>
2022-04-28 22:00:13 -0500

    created associations between user and post

--- models/index.js
+++ models/index.js
@@ -1,6 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// * create associations between User and Post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User,{
+    foreignKey: 'user_id'
+});
+
 module.exports = {
     User,
     Post
