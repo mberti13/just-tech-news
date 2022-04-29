4fd5c60de2cf77e110ac0a578b6a68828c8f0f41
MJB <matt.berti13@gmail.com>
2022-04-29 07:44:50 -0500

    created post and put routes for post model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -45,6 +45,44 @@ router.get('/:id', (req, res) =>{
     });
 });
 
+// CREATE/POST a post
+router.post('/', (req, res) =>{
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// PUT/UPDATE a post's title
+router.put('/:id', (req, res) =>{
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where:{
+                id: req.params.id
+            }
+        }
+    ).then(dbPostData =>{
+        if(!dbPostData){
+            res.status(404).json({ message: 'No post found with this id.' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 
 module.exports = router;
