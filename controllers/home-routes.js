55830288c1084523e51e4759219d7051a0344f25
MJB <matt.berti13@gmail.com>
2022-05-02 16:55:20 -0500

    linked stylesheet to homepage with hardcoded values

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,7 +10,17 @@ const router = require('express').Router();
 router.get('/', (req, res) =>{
     // res.render() works with template engine
     // renders homepage.handlebars
-    res.render('homepage');
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: "Handlebars Docs",
+        created_at: new Date(),
+        vote_count: 10,
+        comment: [{}, {}],
+        user:{
+            username: 'test_user'
+        }
+    });
 });
 
 module.exports = router;
