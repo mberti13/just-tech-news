21d6f7963d62fde99ef2b2fe188e296639a8d0e2
MJB <matt.berti13@gmail.com>
2022-05-04 18:40:08 -0500

    finished front-end logic for logging in

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,4 @@
-function signupFormHandler(event){
+async function signupFormHandler(event){
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
@@ -9,7 +9,7 @@ function signupFormHandler(event){
     // * submit a post request to the /api/users endpoint
     // * adding the user information to the database
     if(username && email && password){
-        fetch('/api/users', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
@@ -18,10 +18,40 @@ function signupFormHandler(event){
             }),
 
             headers: { 'Content-Type': 'application/json' }
-            
-        }).then((response) =>{console.log(response)})
+
+        });
+        // ! Check the response status
+        if(response.ok){
+            console.log('success');
+        }
+        alert(response.statusText);
     }
 
 }
 
+async function loginFormHandler(event){
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if(email && password){
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+
+            headers: { 'Content-Type': 'application/json'}
+        });
+        if(response.ok){
+            document.location.replace('/');
+        }else{
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
