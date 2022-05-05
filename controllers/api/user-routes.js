9e6f3f227c127ad781f35ad250709377aaaa02cd
MJB <matt.berti13@gmail.com>
2022-05-04 19:51:46 -0500

    added ability to end session/logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -108,6 +108,22 @@ router.post('/login', (req, res) => {
 
 });
 
+// * POST route for users to logout
+// * Destroys the user's session variables
+router.post('/logout', (req, res) =>{
+    if(req.session.loggedIn){
+        req.session.destroy(() =>{
+            // ? 204 Code means success (No Content)
+            // ? Save and Continue editing functionality
+            // ! states logout was a success
+            res.status(204).end();
+        });
+    }
+    else{
+        res.status(404).end();
+    }
+});
+
 // * PUT /api/users/1
 router.put('/:id', (req, res) => {
     // * expects {username, email, password}
